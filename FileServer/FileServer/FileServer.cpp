2b400a594f109fbd2e87e2d026e76c68cb3da521
Захар Голуб <pahargolub2@gmail.com>
2025-07-26 22:45:16 +0300

    Разработка файлового сервера на C++23

---
+++ FileServer/FileServer/FileServer.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <print>
+#include <string>
+#include <vector>
+
+std::vector<std::string> programArgs;
+
+int main(int argc, char** argv)
+{
+	for(int i=0;i!=argc;++i)
+		programArgs.push_back(argv[i]);
+
+
+}
