69f5bb485e1e4c2ec2442258f1266fb30912c91f
Захар Голуб <pahargolub2@gmail.com>
2025-08-02 13:04:44 +0300

    Вывод версии OpenSSL и MySQL connector

--- SameFileServer/FileServer/src/main.cpp
+++ SameFileServer/FileServer/src/main.cpp
@@ -12,6 +12,8 @@
 
 
 #include <mysqlx/xdevapi.h>
+#include <mysqlx/version_info.h>
+#include <mysqlx/common_constants.h>
 
 
 
@@ -23,6 +25,15 @@
 
 int main()
 {
+	srand(time(NULL));
+	setlocale(LC_ALL, "Russian");
+
+	std::cout<<"OpenSSL version: "<<OPENSSL_VERSION_TEXT<<std::endl;
+
+	std::cout<<"MySQL connector version: "<< MYSQL_CONCPP_NAME <<
+	'\t' << MYSQL_CONCPP_VERSION <<
+	"\tLicense:" <<MYSQL_CONCPP_LICENSE << std::endl;
+
 	Socket::InitTheWSA();
 	std::mutex printMutex;
 	
@@ -48,7 +59,8 @@ int main()
 					SecureSocket server(AF_INET, ConnectionType::TCP, 54321, key);
 					
 					printMutex.lock();
-					std::cout << "Waiting for connection on port 54321..." << std::endl;
+					std::cout <<
+					"Waiting for connection on port 54321..." << std::endl;
 					printMutex.unlock();
 					
 					// Ожидание подключения и получение данных
@@ -67,7 +79,9 @@ int main()
 							auto request = server.ReceiveJson();
 							
 							printMutex.lock();
-							std::cout << "Received JSON: " << request.dump(2) << std::endl;
+							std::cout <<
+							"Received JSON: "<<
+							request.dump(2) << std::endl;
 							printMutex.unlock();
 							
 							// Подготовка ответа
@@ -75,20 +89,23 @@ int main()
 							{
 								{"status", "success"},
 								{"message", "Data processed successfully"},
-								{"received_size", server.GetBufferedString().size()}
+								{"received_size", 
+								server.GetBufferedString().size()}
 							};
 							
 							// Отправка ответа
 							if (!server.SendJson(response))
 							{
 								std::lock_guard<std::mutex> lock(printMutex);
-								std::cerr << "Failed to send response" << std::endl;
+								std::cerr <<
+								"Failed to send response" << std::endl;
 							}
 						}
 						catch (const std::exception& e)
 						{
 							std::lock_guard<std::mutex> lock(printMutex);
-							std::cerr << "Processing error: " << e.what() << std::endl;
+							std::cerr <<
+							"Processing error: " << e.what() << std::endl;
 							
 							try
 							{
@@ -101,7 +118,8 @@ int main()
 							}
 							catch (...)
 							{
-								std::cerr << "Failed to send error response" << std::endl;
+								std::cerr <<
+								"Failed to send error response" << std::endl;
 							}
 						}
 					}
@@ -129,119 +147,3 @@ int main()
 	Socket::CleanTheWSA();
 	return 0;
 }
-
-//void run_server() {
-//	try {
-//		// Используем фиксированный ключ для тестирования
-//		std::array<uint8_t, 32> key = {
-//			0x33, 0x32, 0x2d, 0x62, 0x79, 0x74, 0x65, 0x2d,
-//			0x6c, 0x6f, 0x6e, 0x67, 0x2d, 0x65, 0x6e, 0x63,
-//			0x72, 0x79, 0x70, 0x74, 0x69, 0x6f, 0x6e, 0x2d,
-//			0x6b, 0x65, 0x79, 0x2d, 0x31, 0x32, 0x33, 0x34
-//		};
-//
-//		SecureSocket server(AF_INET, ConnectionType::TCP, 54321, key);
-//		server.Listen();
-//
-//		std::cout << "Server started. Waiting for connections..." << std::endl;
-//
-//		while (true) {
-//			if (server.Accept() == 0) {
-//				try {
-//					std::cout << "Client connected" << std::endl;
-//
-//					// Прием и вывод отладочной информации
-//					auto request = server.ReceiveJson();
-//					std::cout << "Received JSON: " << request.dump() << std::endl;
-//
-//					// Простой ответ
-//					nlohmann::json response = {
-//						{"status", "success"},
-//						{"message", "Data received successfully"}
-//					};
-//					server.SendJson(response);
-//				}
-//				catch (const std::exception& e) {
-//					std::cerr << "Processing error: " << e.what() << std::endl;
-//
-//					try {
-//						nlohmann::json error_resp = {
-//							{"status", "error"},
-//							{"message", e.what()}
-//						};
-//						server.SendJson(error_resp);
-//					}
-//					catch (...) {
-//						std::cerr << "Failed to send error response" << std::endl;
-//					}
-//				}
-//				server.CloseClientSocket();
-//			}
-//		}
-//	}
-//	catch (const std::exception& e) {
-//		std::cerr << "Server fatal error: " << e.what() << std::endl;
-//	}
-//}
-//
-//
-//std::vector<std::string> programArgs;
-//std::mutex printMutex;
-//
-//int main(int argc, char** argv)
-//{
-//	for(int i=0;i!=argc;++i)
-//		programArgs.push_back(argv[i]);
-//
-//	setlocale(LC_ALL, "Russian");
-//
-//	Socket::InitTheWSA();
-//	std::cout << "OpenSSL version: " << OPENSSL_VERSION_TEXT << std::endl;
-//
-//	std::array<uint8_t, 32> key =
-//	{
-//		0x32, 0x2d, 0x62, 0x79, 0x74, 0x65, 0x2d, 0x6c,
-//		0x6f, 0x6e, 0x67, 0x2d, 0x65, 0x6e, 0x63, 0x72,
-//		0x79, 0x70, 0x74, 0x69, 0x6f, 0x6e, 0x2d, 0x6b,
-//		0x65, 0x79, 0x2d, 0x31, 0x32, 0x33, 0x34, 0x00
-//	};
-//
-//
-//	std::thread thread_for_default_connection ([&]()
-//	{
-//		while (true)
-//		{
-//			try
-//			{
-//				Socket socket(AF_INET, ConnectionType::TCP, 8000);
-//				printMutex.lock();
-//				std::println("new iteration of default connection");
-//				printMutex.unlock();
-//				int res = socket.Listen();
-//				if (!res)
-//				{
-//					printMutex.lock();
-//					std::println("{}",socket.GetBufferedString());
-//					printMutex.unlock();
-//				}
-//			}
-//			catch (...)
-//			{
-//			}
-//		}
-//	});
-//	std::thread thread_for_encrypt_connection([&]()
-//	{
-//		run_server();
-//	});
-//	thread_for_encrypt_connection.join();
-//	thread_for_default_connection.join();
-//
-//#ifdef _WIN32
-//	system("pause");
-//#elifdef __unix__
-//	printf("Press the \"Enter\" key to continue\n");
-//	getchar();
-//#endif
-//	Socket::CleanTheWSA();
-//}
