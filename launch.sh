b1baf72ce964c70e1bbc6b9e6e07fc51198efa07
Захар Голуб <pahargolub2@gmail.com>
2025-09-01 14:41:50 +0300

    фикс запуска виртуального окружения

--- launch.sh
+++ launch.sh
@@ -1,6 +1,10 @@
+python3 -m venv .venv && \
 . .ven/bin/activate && \
+
 git stash && \
 git pull origin master &&  \
+
 chmod +x *.sh && \
 ./setup.sh && \
+
 while true; do python3 FrontendServer/src/backend/app.py ; done
