5cf36cc993e9837658d2eec635fcaf6e0eb38a1a
Захар Голуб <pahargolub2@gmail.com>
2025-09-01 14:24:25 +0300

    Скрипт установки

---
+++ setup.sh
@@ -0,0 +1,11 @@
+sudo apt-get install mysql-server
+
+sudo apt install python3.12-venv
+python3 -m venv .venv
+. .venv/bin/activate
+
+
+pip install Flask mysql-connector-python bcrypt Flask-JWT-Extended markupsafe flask_wtf
+
+. .venv/bin/activate
+
