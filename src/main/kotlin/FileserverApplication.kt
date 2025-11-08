2db3b90cfa0d8204dbffda595c7b68455d0abe98
ClassZak <pahargolub2@gmail.com>
2025-11-08 13:42:16 +0300

    Coping old frontend files

--- src/main/kotlin/FileserverApplication.kt
+++ src/main/kotlin/FileserverApplication.kt
@@ -7,6 +7,5 @@ import org.springframework.boot.runApplication
 class FileserverApplication
 
 fun main(args: Array<String>) {
-    println("Sugoma!")
     runApplication<FileserverApplication>(*args)
 }
