b7861c19aa802d79ec740a4b2ab59c8e9fa27d52
ClassZak <pahargolub2@gmail.com>
2025-12-04 18:55:23 +0300

    Fix footer

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import AccountPage from "./pages/AccountPage";
 import Header from "./parts/Header";
 import Footer from './parts/Footer';
 import FilesPage from './pages/FilesPage';
+import './App.css'; // Создадим этот файл
 
 function Container({ children }) {
 	return <div className="max-w-5xl mx-auto px-6 py-10">{children}</div>;
@@ -23,35 +24,38 @@ function NotFound() {
 	);
 }
 
-export default function App() {
+function App() {
 	return (
-		<div>
-			<Header />
+		<div className="app-container">
 			<AuthProvider>
 				<Router>
-					<Routes>
-						{/* Публичные маршруты */}
-						<Route path="/" element={<HomePage />} />
-						<Route path="/about" element={<AboutPage />} />
-						<Route path="/login" element={<LoginPage />} />
-						
-						{/* Защищенные маршруты */}
-						<Route path="/account" element={
-							<ProtectedRoute>
-								<AccountPage />
-							</ProtectedRoute>
-						} />
-						
-						<Route path="/files*" element={
-							<ProtectedRoute>
-								<FilesPage />
-							</ProtectedRoute>
-						} />
-						<Route path="*" element={<Navigate to="/" replace />} />
-					</Routes>
+					<Header />
+					<main className="main-wrapper">
+						<Routes>
+							<Route path="/" element={<HomePage />} />
+							<Route path="/about" element={<AboutPage />} />
+							<Route path="/login" element={<LoginPage />} />
+							
+							<Route path="/account" element={
+								<ProtectedRoute>
+									<AccountPage />
+								</ProtectedRoute>
+							} />
+							
+							<Route path="/files/*" element={
+								<ProtectedRoute>
+									<FilesPage />
+								</ProtectedRoute>
+							} />
+							
+							<Route path="*" element={<Navigate to="/" replace />} />
+						</Routes>
+					</main>
+					<Footer />
 				</Router>
 			</AuthProvider>
-			<Footer />
 		</div>
 	);
 }
+
+export default App;
