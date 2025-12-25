27221753626b7b4ff43c8ea58654d8477cb4ca87
ClassZak <pahargolub2@gmail.com>
2025-12-25 19:45:17 +0300

    Update About and Home pages

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,7 +6,7 @@ function HomePage(){
 		<MainContent>
 			<div>
 				<img src={factory} style={{display: "flex"}} alt="Логотип предприятия"></img>
-				<h1>Файловый сервер ООО "hfghfgh"</h1>
+				<h1>Файловый сервер ООО "ClassZak Engine"</h1>
 			</div>
 		</MainContent>
 	);
