da1788ed22ec0660aa94a411c3a5c58a821d7bdd
ClassZak <pahargolub2@gmail.com>
2026-01-29 22:45:44 +0300

    Add about page. Need to fix page height

--- angular_server/src/app/app.routes.ts
+++ angular_server/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { About } from './page/about/about';
+import { Home } from './page/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{ path: '', component: Home},
+	{ path: 'about', component: About},
+	{ path: '', redirectTo: '/home', pathMatch: 'full'}
+];
---
+++ angular_server/src/app/page/about/about.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { AppFooter } from '../../app-footer/app-footer'
+
+@Component({
+  selector: 'app-about',
+  imports: [AppFooter],
+  templateUrl: './about.html',
+  styleUrl: './about.css',
+})
+export class About {
+
+}
