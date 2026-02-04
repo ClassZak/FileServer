7f03840f501215b764baf125d7a46bc1eb80be9b
ClassZak <pahargolub2@gmail.com>
2026-02-04 09:03:18 +0300

    Fix header and add images

---
+++ angular_server/src/app/app-header/app-header.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { FileSearch } from "../file-search/file-search";
+
+@Component({
+  selector: 'app-app-header',
+  imports: [FileSearch],
+  templateUrl: './app-header.html',
+  styleUrl: './app-header.css',
+})
+export class AppHeader {
+
+}
--- angular_server/src/app/page/about/about.ts
+++ angular_server/src/app/page/about/about.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { AppFooter } from '../../app-footer/app-footer'
+import { AppHeader } from "../../app-header/app-header";
 
 @Component({
   selector: 'app-about',
-  imports: [AppFooter],
+  imports: [AppFooter, AppHeader],
   templateUrl: './about.html',
   styleUrl: './about.css',
 })
