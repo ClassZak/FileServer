c47a5f32891fdf6285711e45ba522d9865f31af6
ClassZak <pahargolub2@gmail.com>
2026-02-04 11:35:27 +0300

    Add home page

--- angular_server/src/app/page/home/home.ts
+++ angular_server/src/app/page/home/home.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { AppFooter } from '../../app-footer/app-footer'
+import { AppHeader } from "../../app-header/app-header";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [AppFooter, AppHeader],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
