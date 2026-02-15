506913baf6e2673c5d9ecf6e4a4f41d4851b83a2
ClassZak <pahargolub2@gmail.com>
2026-02-15 20:46:21 +0300

    Fix tables styles

--- angular_server/src/app/file-search/file-search.ts
+++ angular_server/src/app/file-search/file-search.ts
@@ -1,11 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
 	selector: 'app-file-search',
-	imports: [],
+	imports: [CommonModule, FormsModule],
 	templateUrl: './file-search.html',
 	styleUrl: './file-search.css',
 })
 export class FileSearch {
+	public isSearching: boolean = false;
+	public searchQuery: string = '';
 
+	constructor (
+		private router: Router
+	){}
+	
+	handleSearch(): void {
+		const query = this.searchQuery.trim();
+		if (!query) {
+			return; // ничего не делаем при пустом запросе
+		}
+
+		// Переход в корень с параметрами поиска
+		this.router.navigate(['/files'], {
+			queryParams: {
+				q: query,
+				searchPath: '' // поиск от корня
+			}
+		});
+	}
 }
