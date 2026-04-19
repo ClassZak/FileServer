dc8ed4bb836a5ff13f5ef7df7384703fff77a99c
ClassZak <pahargolub2@gmail.com>
2026-04-19 07:02:21 +0300

    Add test

--- angular_server/src/app/component/notice-component/notice-component.spec.ts
+++ angular_server/src/app/component/notice-component/notice-component.spec.ts
@@ -3,21 +3,22 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NoticeComponent } from './notice-component';
 
 describe('NoticeComponent', () => {
-  let component: NoticeComponent;
-  let fixture: ComponentFixture<NoticeComponent>;
+	let component: NoticeComponent;
+	let fixture: ComponentFixture<NoticeComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [NoticeComponent]
-    })
-    .compileComponents();
+	beforeEach(async () => {
+		TestBed.resetTestingModule();
+		await TestBed.configureTestingModule({
+			imports: [NoticeComponent]
+		})
+		.compileComponents();
 
-    fixture = TestBed.createComponent(NoticeComponent);
-    component = fixture.componentInstance;
-    await fixture.whenStable();
-  });
+		fixture = TestBed.createComponent(NoticeComponent);
+		component = fixture.componentInstance;
+		await fixture.whenStable();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
