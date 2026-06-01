84b3eb676d0c46bd371fa8ed628e34704ecfd99b
ClassZak <156465942+ClassZak@users.noreply.github.com>
2026-06-01 09:48:28 +0300

    Update and rename rebuild.sh to rebuild_and_run.sh

--- rebuild_and_run.sh
+++ rebuild_and_run.sh
@@ -7,4 +7,4 @@ npm run build &&
 rm -rf "$REPO_DIR/kotlin_spring_server/src/main/resources/static/"* &&
 cp -r "$REPO_DIR/angular_server/dist/file_server/browser/"* "$REPO_DIR/kotlin_spring_server/src/main/resources/static/" &&
 cd "$REPO_DIR/kotlin_spring_server/" &&
-./gradlew bootRun > ~/kotlin_logs.log
+./gradlew bootRun > ~/kotlin_logs.log &
